c1f3abf6e1da2b0ef4277c493820350355163ca4
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-05-13 21:40:35 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const noButton = document.getElementById('noButton');
-    
+    const container = document.querySelector('.container');
+
     noButton.addEventListener('mouseover', () => {
-        const container = document.querySelector('.container');
         const containerRect = container.getBoundingClientRect();
         const buttonRect = noButton.getBoundingClientRect();
 
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const randomX = Math.floor(Math.random() * maxX);
         const randomY = Math.floor(Math.random() * maxY);
 
-        noButton.style.position = 'absolute';
         noButton.style.left = `${randomX}px`;
         noButton.style.top = `${randomY}px`;
-    }
+    });
+});
